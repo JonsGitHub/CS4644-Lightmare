eaa264c39a4537f20f78d9d32b129499b37f9244
JSzajek <JSzajek@gmail.com>
2021-03-09 21:00:42 -0500

    Lightmare-1-Added basic player controller, testing scene, and pause menu.

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour
+{
+    private enum MouseState
+    {
+        Locked, Unlocked
+    }
+    
+    private MouseState CurrentState;
+    private GameObject Fade;
+    private GameObject PauseMenu;
+
+    void Awake()
+    {
+        Fade = transform.Find("Fade").gameObject;
+        PauseMenu = transform.Find("Pause_Menu").gameObject;
+        
+        Unpause();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (CurrentState == MouseState.Unlocked)
+            {
+                Unpause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void OnContinuePressed()
+    {
+        Unpause();
+    }
+
+    private void Unpause()
+    {
+        PauseMenu.SetActive(false);
+        Fade.SetActive(false);
+
+        CurrentState = MouseState.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+        Time.timeScale = 1;
+    }
+
+    private void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Fade.SetActive(true);
+
+        CurrentState = MouseState.Unlocked;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        
+        Time.timeScale = 0;
+    }
+
+    
+}
