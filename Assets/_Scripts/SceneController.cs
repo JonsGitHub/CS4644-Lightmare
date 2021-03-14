56b4f9279418127d8ec294b7351889eb37544164
JSzajek <JSzajek@gmail.com>
2021-03-14 01:15:19 -0500

    Lightmare-3-Implemented basic npc movement/following.

--- Assets/_Scripts/SceneController.cs
+++ Assets/_Scripts/SceneController.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Scene Controller class managing loading and saving of a scene
 /// </summary>
-public class SceneController : MonoBehaviour, ISceneController
+public abstract class SceneController : MonoBehaviour
 {
     /// <summary>
     /// Awake called before Start of class
@@ -13,33 +13,15 @@ public class SceneController : MonoBehaviour, ISceneController
         tag = "SceneController";
     }
 
-    /// <inheritdoc/>
-    public virtual void Load(object data)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    /// <inheritdoc/>
-    public virtual SceneData Save()
-    {
-        throw new System.NotImplementedException();
-    }
-}
-
-/// <summary>
-/// Interface representing required methods for a scene controller
-/// </summary>
-public interface ISceneController
-{
-    /// <summary>
-    /// Saves the current scene state.
-    /// </summary>
-    /// <returns>The current scene state's data</returns>
-    SceneData Save();
-
     /// <summary>
     /// Loads the scene data into the scene.
     /// </summary>
     /// <param name="data">The data to load</param>
-    void Load(object data);
+    public abstract void Load(object data);
+
+    /// <summary>
+    /// Saves the current scene state.
+    /// </summary>
+    /// <returns>The current scene state's data</returns>
+    public abstract SceneData Save();
 }
