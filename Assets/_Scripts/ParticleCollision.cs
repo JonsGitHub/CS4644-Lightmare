beef40108cd37e30d0e613ebc01567175b07ef08
JonsGitHub <jonathancarter101@gmail.com>
2021-03-26 01:32:59 -0400

    Added Particle collision system. Destroys on impact, WIP on damaging "damageable" entities. Still need to rig to player

---
+++ Assets/_Scripts/ParticleCollision.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollision : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        for (int i = 0; i < numCollisionEvents; i++)
+        {
+            SendDamageMessage(other);
+        }
+
+        //Rigidbody rb = other.GetComponent<Rigidbody>();
+        //int i = 0;
+
+        //while (i < numCollisionEvents)
+        //{
+        //    if (rb)
+        //    {
+        //        Vector3 pos = collisionEvents[i].intersection;
+        //        Vector3 force = collisionEvents[i].velocity * 10;
+        //        rb.AddForce(force);
+        //    }
+        //    i++;
+        //}
+        //}
+    }
+
+    void SendDamageMessage(GameObject other)
+    {
+        var d = other.GetComponent<Damageable>();
+        if (!d)
+            return;
+        d.ReceiveAnAttack(3); //Fixed damage amount of 3, change later
+    }
+}
