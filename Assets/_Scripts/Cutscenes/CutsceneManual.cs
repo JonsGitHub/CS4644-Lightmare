19330118d4e91ad87cb787c6380d0d43b6cfc10a
JSzajek <JSzajek@gmail.com>
2021-03-30 16:11:33 -0400

    Lightmare-9-Added basic outline for interaction puzzles.

---
+++ Assets/_Scripts/Cutscenes/CutsceneManual.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class to manually trigger a cutscene.
+/// </summary>
+
+public class CutsceneManual : MonoBehaviour
+{
+	[SerializeField] private CutsceneController _playableCutscene = default;
+	[SerializeField] private bool _playOnStart = default;
+	[SerializeField] private bool _playOnce = default;
+
+	[Header("Broadcasting on channels")]
+	[SerializeField] private PlayCutsceneChannelSO _playCutsceneEvent = default;
+
+	private void Start()
+	{
+		if (_playOnStart)
+			PlayCutScene();
+	}
+
+	public void PlayCutScene()
+    {
+		_playCutsceneEvent?.RaiseEvent(_playableCutscene);
+
+		if (_playOnce)
+			Destroy(this);
+	}
+}
---
+++ Assets/_Scripts/Events/ScriptableObjects/UI/PuzzleChannelSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Events/Puzzel Event Channel")]
+public class PuzzleChannelSO : ScriptableObject
+{
+	public UnityAction<Puzzle> OnEventRaised;
+	
+	public void RaiseEvent(Puzzle puzzle)
+	{
+		OnEventRaised?.Invoke(puzzle);
+	}
+}
