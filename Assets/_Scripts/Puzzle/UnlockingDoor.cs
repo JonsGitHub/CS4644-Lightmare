b824119390a8f90088cc3255d7609515cb700db1
JSzajek <JSzajek@gmail.com>
2021-03-31 16:51:19 -0400

    Lightmare-9-Added more assets to tutorial level.

--- Assets/_Scripts/Puzzle/UnlockingDoor.cs
+++ Assets/_Scripts/Puzzle/UnlockingDoor.cs
@@ -6,15 +6,30 @@ public class UnlockingDoor : UnlockingBase
 {
     [Tooltip("Movement Config of the path to lock/unlock door position. Where the first is the locked position and second is the unlocked position.")]
     [SerializeField] private PathwayConfigSO _movementConfig;
+    [SerializeField] private Animator _animator;
 
     public override void Lock()
     {
-        StartCoroutine(LerpPosition(_movementConfig.Waypoints.First().waypoint));
+        if (_animator)
+        {
+            _animator.Play("Lock");
+        }
+        else
+        {
+            StartCoroutine(LerpPosition(_movementConfig.Waypoints.First().waypoint));
+        }
     }
 
     public override void Unlock()
     {
-        StartCoroutine(LerpPosition(_movementConfig.Waypoints.Last().waypoint));
+        if (_animator)
+        {
+            _animator.Play("Unlock");
+        }
+        else
+        {
+            StartCoroutine(LerpPosition(_movementConfig.Waypoints.Last().waypoint));
+        }
     }
 
     IEnumerator LerpPosition(Vector3 endPosition)
