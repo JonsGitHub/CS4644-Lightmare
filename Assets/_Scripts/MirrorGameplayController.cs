b1c92978517e7eb99e297522f7b14c70f7449893
JSzajek <JSzajek@gmail.com>
2021-04-10 04:47:39 -0400

    Lightmare-13-Re-polish to tutorial level and implementation of mirroring.

---
+++ Assets/_Scripts/MirrorGameplayController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class MirrorGameplayController : MonoBehaviour
+{
+    [SerializeField] private MirrorLightController _mirrorLight;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private List<VisualEffect> _visualEffects;
+
+    public void PlayTransition(string name) => _animator.Play(name);
+
+    public void SetMirrorWalkingState(int state) => _mirrorLight.SetBoolParameter("IsWalking", state == 1);
+
+    public void SetMirrorPosingState(int state) => _mirrorLight.SetBoolParameter("IsPosing", state == 1);
+
+    private void PlayVisualEffect(int index) => _visualEffects.ElementAtOrDefault(index)?.Play();
+}
