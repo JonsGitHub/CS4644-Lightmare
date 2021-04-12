29785f833081fdc2335839ce1a4cde9d45cfbdf5
JSzajek <JSzajek@gmail.com>
2021-04-12 06:21:20 -0400

    Lightmare-14-Final touch ups and quick skybox area effect.

---
+++ Assets/_Scripts/MisfortuneCutsceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MisfortuneCutsceneController : MonoBehaviour
+{
+    [SerializeField] private Exploder _exploder;
+    [SerializeField] private List<Exploder> _explodingObelisks;
+    [SerializeField] private LocationExit _exit;
+
+    [SerializeField] private SkyboxManager _skyboxManager;
+
+    public void ExplodeBarrier() => _exploder.Explode();
+
+    public void ExplodeObelisk(int index) => _explodingObelisks[index].Explode();
+
+    public void LoadNextLevel() => _exit.ManualTrigger();
+
+    public void SwitchSkybox() => _skyboxManager.SwitchToSecondSkybox();
+}
